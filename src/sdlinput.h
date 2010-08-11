ccf95328cf1ae1a3c913aed252ca16ac828a4ffa
Sam Hocevar <sam@hocevar.net>
2010-08-11 16:18:48 +0000

    Reimplement the SDL input as a ticked asset object.

---
+++ src/sdlinput.h
@@ -0,0 +1,34 @@
+//
+// Deus Hax (working title)
+// Copyright (c) 2010 Sam Hocevar <sam@hocevar.net>
+//
+
+//
+// The SdlInput class
+// ------------------
+//
+
+#if !defined __DH_SDLINPUT_H__
+#define __DH_SDLINPUT_H__
+
+#include "asset.h"
+#include "game.h"
+
+class SdlInputData;
+
+class SdlInput : public Asset
+{
+public:
+    SdlInput(Game *game);
+    virtual ~SdlInput();
+
+protected:
+    virtual Group GetGroup();
+    virtual void TickGame(float delta_time);
+
+private:
+    SdlInputData *data;
+};
+
+#endif // __DH_SDLINPUT_H__
+
--- src/test-map.cpp
+++ src/test-map.cpp
@@ -13,6 +13,7 @@
 #include <SDL.h>
 
 #include "sdlvideo.h"
+#include "sdlinput.h"
 #include "game.h"
 #include "ticker.h"
 
@@ -21,34 +22,17 @@ int main(int argc, char **argv)
     Video *video = new SdlVideo("Deus Hax", 640, 480);
     Game *game = new Game("maps/testmap.tmx");
 
+    /* Register the input driver */
+    new SdlInput(game);
+
     while (!game->Finished())
     {
-        /* Test stuff */
-        int mx, my;
-        SDL_GetMouseState(&mx, &my);
-        game->SetMouse(mx * (640 - 32) / 640, my * (480 - 32) / 480);
-
         Ticker::TickGame(33.33333f);
 
         video->PreRender();
         Ticker::TickRender(33.33333f);
         game->Render();
         video->PostRender(33.33333f);
-
-        /* This could go in a separate function */
-        SDL_Event event;
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-                game->Quit();
-            if (event.type == SDL_KEYDOWN)
-            {
-                if (event.key.keysym.sym == SDLK_RETURN)
-                    video->FullScreen();
-                else if (event.key.keysym.sym == SDLK_ESCAPE)
-                    game->Quit();
-            }
-        }
     }
 
     delete game;
