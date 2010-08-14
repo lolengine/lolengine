932b60a12345aa2d97da7e0fa8ea237b5378b3d6
Sam Hocevar <sam@hocevar.net>
2010-08-14 21:15:44 +0000

    Proper delta time computation in both the game and the editor.

--- src/debugfps.cpp
+++ src/debugfps.cpp
@@ -20,7 +20,10 @@ class DebugFpsData
 {
     friend class DebugFps;
 
+    static int const HISTORY = 30;
+
 private:
+    float history[HISTORY];
     Font *font;
     int frame;
 };
@@ -33,6 +36,8 @@ DebugFps::DebugFps()
 {
     data = new DebugFpsData();
 
+    for (int i = 0; i < DebugFpsData::HISTORY; i++)
+        data->history[i] = 0.0f;
     data->font = Forge::GetFont("gfx/font/ascii.png");
     data->frame = 0;
 }
@@ -46,8 +51,21 @@ void DebugFps::TickRender(float delta_time)
 {
     Asset::TickGame(delta_time);
 
+    data->history[data->frame % DebugFpsData::HISTORY] = delta_time;
+    data->frame++;
+
+    float mean = 0.0f, max = 0.0f;
+    for (int i = 0; i < DebugFpsData::HISTORY; i++)
+    {
+        mean += data->history[i];
+        if (data->history[i] > max)
+            max = data->history[i];
+    }
+    mean /= DebugFpsData::HISTORY;
+
     char buf[1024];
-    sprintf(buf, "%3.2f fps (%i)", 1000.0f / delta_time, data->frame++);
+    sprintf(buf, "%3.2f ms (%3.2f fps) -- max %3.2f ms -- #%i",
+            mean, 1000.0f / mean, max, data->frame);
     data->font->Print(10, 10, buf);
     data->font->Print(11, 10, buf);
     data->font->Print(10, 11, buf);
