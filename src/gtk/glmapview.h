ea059c36e84d6792de1b05f12b3b51cf9a8115d7
Sam Hocevar <sam@hocevar.net>
2010-08-22 16:02:20 +0000

    Refactor editor to get the glarea object out of the main source file.

---
+++ src/gtk/glmapview.h
@@ -0,0 +1,49 @@
+//
+// Deus Hax (working title)
+// Copyright (c) 2010 Sam Hocevar <sam@hocevar.net>
+//
+
+#if !defined __DH_GLMAPVIEW_H__
+#define __DH_GLMAPVIEW_H__
+
+#include <gtk/gtk.h>
+
+class GlMapView
+{
+public:
+    GlMapView(GtkBuilder *builder);
+    void LoadMap(char const *path);
+
+private:
+    /* Private methods */
+    gboolean IdleTick();
+    gboolean Setup();
+    gboolean Destroy();
+    gboolean Draw(GdkEventExpose *event);
+    gboolean MouseButton(GdkEventButton *event);
+    gboolean MouseMotion(GdkEventMotion *event);
+
+    /* Private signal slots */
+    static gboolean IdleTickSignal(GlMapView *that);
+    static gboolean SetupSignal(GtkWidget *w, GlMapView *that);
+    static gboolean DestroySignal(GtkWidget *w, GlMapView *that);
+    static gboolean DrawSignal(GtkWidget *w, GdkEventExpose *event,
+                               GlMapView *that);
+    static gboolean ReshapeSignal(GtkWidget *w, GdkEventConfigure *event,
+                                  GlMapView *that);
+    static gboolean MouseButtonSignal(GtkWidget *w, GdkEventButton *event,
+                                      GlMapView *that);
+    static gboolean MouseMotionSignal(GtkWidget *w, GdkEventMotion *event,
+                                      GlMapView *that);
+
+private:
+    GtkAdjustment *hadj, *vadj;
+    GtkWidget *glarea;
+    gboolean ticking, panning;
+
+    MapViewer *mapviewer;
+    double xpan, ypan;
+};
+
+#endif // __DH_GLMAPVIEW_H__
+
