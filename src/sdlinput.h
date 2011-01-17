0ed566d3cc50b5bd8c1e551c0c0292c7e464cce5
Sam Hocevar <sam@hocevar.net>
2011-01-17 23:05:18 +0000

    Complete Lol Engine / Deus Hax / Monsterz split.

--- src/sdlinput.h
+++ src/sdlinput.h
@@ -1,6 +1,11 @@
 //
-// Deus Hax (working title)
-// Copyright (c) 2010 Sam Hocevar <sam@hocevar.net>
+// Lol Engine
+//
+// Copyright: (c) 2010-2011 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
 //
 
 //
@@ -12,7 +17,6 @@
 #define __DH_SDLINPUT_H__
 
 #include "entity.h"
-#include "game.h"
 
 class SdlInputData;
 
--- src/tileset.h
+++ src/tileset.h
@@ -1,6 +1,11 @@
 //
-// Deus Hax (working title)
-// Copyright (c) 2010 Sam Hocevar <sam@hocevar.net>
+// Lol Engine
+//
+// Copyright: (c) 2010-2011 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
 //
 
 //
@@ -23,7 +28,7 @@ class TileSetData;
 class TileSet : public Entity
 {
 public:
-    TileSet(char const *path, int size);
+    TileSet(char const *path, int w, int h);
     virtual ~TileSet();
 
 protected:
