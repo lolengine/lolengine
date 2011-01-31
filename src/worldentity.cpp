de3da713313aca3dc60256945fb6cc593490ea29
Sam Hocevar <sam@hocevar.net>
2011-01-31 01:50:48 +0000

    Create the WorldEntity class, for objects with world coordinates.

---
+++ src/worldentity.cpp
@@ -0,0 +1,46 @@
+//
+// Lol Engine
+//
+// Copyright: (c) 2010-2011 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
+//
+
+#if defined HAVE_CONFIG_H
+#   include "config.h"
+#endif
+
+#include <cstdlib>
+#include <cstdio>
+
+#include "core.h"
+
+/*
+ * Public WorldEntity class
+ */
+
+WorldEntity::WorldEntity()
+{
+}
+
+WorldEntity::~WorldEntity()
+{
+}
+
+char const *WorldEntity::GetName()
+{
+    return "<worldentity>";
+}
+
+void WorldEntity::TickGame(float deltams)
+{
+    Entity::TickGame(deltams);
+}
+
+void WorldEntity::TickDraw(float deltams)
+{
+    Entity::TickDraw(deltams);
+}
+
---
+++ src/worldentity.h
@@ -0,0 +1,40 @@
+//
+// Lol Engine
+//
+// Copyright: (c) 2010-2011 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
+//
+
+//
+// The GraphicEntity class
+// -----------------------
+//
+
+#if !defined __DH_WORLDENTITY_H__
+#define __DH_WORLDENTITY_H__
+
+#include "entity.h"
+
+class WorldEntity : public Entity
+{
+public:
+    float3 position;
+    float3 rotation;
+    float3 velocity;
+    float3 bbox[2];
+
+protected:
+    WorldEntity();
+    virtual ~WorldEntity();
+
+    virtual char const *GetName();
+
+    virtual void TickGame(float deltams);
+    virtual void TickDraw(float deltams);
+};
+
+#endif // __DH_WORLDENTITY_H__
+
