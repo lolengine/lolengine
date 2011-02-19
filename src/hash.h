07ebcb6e292ddb30e7116726573f06bd231e5dcd
Sam Hocevar <sam@hocevar.net>
2011-02-19 21:09:31 +0000

    Implement CRC32 in the new Hash class. Will be useful for Dict and for the new Shader cache.

---
+++ src/hash.h
@@ -0,0 +1,26 @@
+//
+// Lol Engine
+//
+// Copyright: (c) 2010-2011 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
+//
+
+//
+// The Hash class
+// --------------
+//
+
+#if !defined __DH_HASH_H__
+#define __DH_HASH_H__
+
+class Hash
+{
+public:
+    static uint32_t Crc32(char const *data);
+};
+
+#endif // __DH_HASH_H__
+
