b33907b706b371a5d54fe3e05e87ded7d00ae756
Sam Hocevar <sam@hocevar.net>
2011-02-26 10:34:03 +0000

    Define a few user profilers.

--- src/profiler.h
+++ src/profiler.h
@@ -31,6 +31,16 @@ public:
         STAT_TICK_GAME,
         STAT_TICK_DRAW,
         STAT_TICK_BLIT,
+        STAT_USER_00,
+        STAT_USER_01,
+        STAT_USER_02,
+        STAT_USER_03,
+        STAT_USER_04,
+        STAT_USER_05,
+        STAT_USER_06,
+        STAT_USER_07,
+        STAT_USER_08,
+        STAT_USER_09,
         STAT_COUNT
     };
 
