94be5216c493ef414dd0b4f4d2bc7e4a9ff15d1a
Sam Hocevar <sam@hocevar.net>
2011-02-28 02:25:58 +0000

    Merge all debug entity includes in <loldebug.h>.

---
+++ src/loldebug.h
@@ -0,0 +1,25 @@
+//
+// Lol Engine
+//
+// Copyright: (c) 2010-2011 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
+//
+
+//
+// Debug entity classes
+// --------------------
+//
+
+#if !defined __DH_LOLDEBUG_H__
+#define __DH_LOLDEBUG_H__
+
+#include "debugfps.h"
+#include "debugrecord.h"
+#include "debugsphere.h"
+#include "debugstats.h"
+
+#endif // __DH_LOLDEBUG_H__
+
