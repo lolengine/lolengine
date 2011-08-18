8501230de58db6a58301f6b3dd0d2709a6d4209a
Sam Hocevar <sam@hocevar.net>
2011-08-18 16:54:20 +0000

    ps3: add a simple PS3 input system that currently merely emulates mouse movements with the right stick and one mouse button with the L1 or R1 buttons.

---
+++ src/ps3input.h
@@ -0,0 +1,42 @@
+//
+// Lol Engine
+//
+// Copyright: (c) 2010-2011 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
+//
+
+//
+// The Ps3Input class
+// ------------------
+//
+
+#if !defined __LOL_PS3INPUT_H__
+#define __LOL_PS3INPUT_H__
+
+#include "entity.h"
+
+namespace lol
+{
+
+class Ps3InputData;
+
+class Ps3Input : public Entity
+{
+public:
+    Ps3Input();
+    virtual ~Ps3Input();
+
+protected:
+    virtual void TickGame(float deltams);
+
+private:
+    Ps3InputData *data;
+};
+
+} /* namespace lol */
+
+#endif // __LOL_PS3INPUT_H__
+
