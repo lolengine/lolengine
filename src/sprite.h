f79a3c52b55e45b5f9fd60d2ff1bbbcf43c87330
Sam Hocevar <sam@hocevar.net>
2011-08-26 17:20:39 +0000

    core: add a Sprite class to try to factor some common logic currently found in the game. Will be greatly improved later.

---
+++ src/sprite.h
@@ -0,0 +1,44 @@
+//
+// Lol Engine
+//
+// Copyright: (c) 2010-2011 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
+//
+
+//
+// The Sprite class
+// ----------------
+//
+
+#if !defined __LOL_SPRITE_H__
+#define __LOL_SPRITE_H__
+
+#include "worldentity.h"
+#include "tileset.h"
+
+namespace lol
+{
+
+class SpriteData;
+
+class Sprite : public WorldEntity
+{
+public:
+    Sprite(TileSet *tileset, int id);
+    virtual ~Sprite();
+
+protected:
+    virtual void TickGame(float deltams);
+    virtual void TickDraw(float deltams);
+
+private:
+    SpriteData *data;
+};
+
+} /* namespace lol */
+
+#endif // __LOL_SPRITE_H__
+
