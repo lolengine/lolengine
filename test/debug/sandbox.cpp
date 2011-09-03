35454b8f94a23f46f53344d4578f95ee3f182d6b
Sam Hocevar <sam@hocevar.net>
2011-09-03 07:47:52 +0000

    test: add an empty sandbox program and update .gitignore.

---
+++ test/debug/sandbox.cpp
@@ -0,0 +1,25 @@
+//
+// Lol Engine - Sandbox program
+//
+// Copyright: (c) 2005-2011 Sam Hocevar <sam@hocevar.net>
+//   This program is free software; you can redistribute it and/or
+//   modify it under the terms of the Do What The Fuck You Want To
+//   Public License, Version 2, as published by Sam Hocevar. See
+//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
+//
+
+#if defined HAVE_CONFIG_H
+#   include "config.h"
+#endif
+
+#include "core.h"
+
+using namespace std;
+using namespace lol;
+
+int main(int argc, char **argv)
+{
+    /* Insert any test code here */
+    return EXIT_SUCCESS;
+}
+
