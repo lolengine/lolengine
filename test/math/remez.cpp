38bb81cfd0770d85d664005f6fc66e4d217477e5
Sam Hocevar <sam@hocevar.net>
2011-10-10 01:33:15 +0000

    test: various improvements to the Remez exchange solver.

--- test/math/remez.cpp
+++ test/math/remez.cpp
@@ -26,26 +26,20 @@ using namespace std;
 /* The function we want to approximate */
 static real myfun(real const &x)
 {
-    static real const a0 = real::R_1;
-    static real const a1 = real(-11184811) >> 26;
-    static real const b1 = real(-1) / real(6);
-    static real const b2 = real(1) / real(120);
     real y = sqrt(x);
-    return sin(y) / y;
+    return (sin(y) - y) / (x * y);
 }
 
 static real myerr(real const &x)
 {
-    return real::R_1;
+    real y = sqrt(x);
+    return re(x * y);
 }
 
-int main(int argc, char **argv)
+int main(void)
 {
-    RemezSolver<6> solver;
-    //solver.Run(0, 1, myfun, myfun, 15);
-    solver.Run(exp((real)-100), real::R_PI * real::R_PI >> 2, myfun, myfun, 15);
-    //solver.Run(-1, 1, myfun, myfun, 15);
-    //solver.Run(0, real::R_PI * real::R_PI >> 4, myfun, myfun, 15);
+    RemezSolver<4> solver;
+    solver.Run(real::R_1 >> 400, real::R_PI_2 * real::R_PI_2, myfun, myerr, 15);
 
     return EXIT_SUCCESS;
 }
