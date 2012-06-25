3389ed69a22f54cb4c67b6ef9ad958002b775864
Sam Hocevar <sam@hocevar.net>
2012-06-25 14:15:07 +0000

    build: move all games to a "games" subdirectory to clean up the root directory layout mess.

--- src/debug/fps.cpp
+++ src/debug/fps.cpp
@@ -44,12 +44,12 @@ DebugFps::DebugFps(int x, int y)
 #if 0
     for (int i = 0; i < 5; i ++)
     {
-        data->lines[i] = new Text(NULL, "gfx/font/ascii.png");
+        data->lines[i] = new Text(NULL, "src/gfx/font/ascii.png");
         data->lines[i]->SetPos(ivec3(x, y + (i ? 8 : 0) + 16 * i, 0));
         Ticker::Ref(data->lines[i]);
     }
 #else
-    data->lines[0] = new Text(NULL, "gfx/font/ascii.png");
+    data->lines[0] = new Text(NULL, "src/gfx/font/ascii.png");
     data->lines[0]->SetPos(ivec3(x, y, 100));
     Ticker::Ref(data->lines[0]);
 #endif
