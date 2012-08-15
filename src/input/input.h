32e01aac3196a1d5d4b4998f7dd0c42bd0483121
Benjamin ‘Touky’ Huet <huet.benjamin@gmail.com>
2012-08-15 21:06:50 +0000

    Fixed build

--- src/input/input.h
+++ src/input/input.h
@@ -108,7 +108,7 @@ struct ActionSetting
 
 	ActionSetting(int NewActionId)
 	{
-		memset(this, 0, sizeof(ActionSetting));
+		//memset(this, 0, sizeof(ActionSetting));
 		ActionId = NewActionId;
 	}
 };
@@ -122,7 +122,7 @@ struct ButtonSetting
 
 	ButtonSetting(int NewRawButtonId)
 	{
-		memset(this, 0, sizeof(ButtonSetting));
+		//memset(this, 0, sizeof(ButtonSetting));
 		RawButtonId = NewRawButtonId;
 	}
 	int GetActionSettingIdx(int ActionId)
