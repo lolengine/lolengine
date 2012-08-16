9372eeae81ee58b912c4fcff96ff7a8db82aca7a
Sam Hocevar <sam@hocevar.net>
2012-08-16 07:11:00 +0000

    build: fix a few compilation and build errors.

--- test/Physics/Src/EasyConstraint.cpp
+++ test/Physics/Src/EasyConstraint.cpp
@@ -1,3 +1,8 @@
+
+#if defined HAVE_CONFIG_H
+#   include "config.h"
+#endif
+
 #include "../Include/LolBtPhysicsIntegration.h"
 #include "../Include/LolPhysics.h"
 #include "../Include/EasyConstraint.h"
