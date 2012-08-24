2af8c07dcfdb750d5b8ee9c78028209f59eb6c29
Sam Hocevar <sam@hocevar.net>
2012-08-24 22:12:34 +0000

    ofsdiofisdoifods

--- src/ticker.cpp
+++ src/ticker.cpp
@@ -165,7 +165,7 @@ void *TickerData::GameThreadMain(void * /* p */)
 
         Profiler::Start(Profiler::STAT_TICK_GAME);
 
-#if 0
+#if 1
         Log::Debug("-------------------------------------\n");
         for (int i = 0; i < Entity::ALLGROUP_END; i++)
         {
