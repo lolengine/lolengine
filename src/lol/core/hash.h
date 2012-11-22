1ee3808602e7510f9bd7c2ac7eb145f2c8db2aad
Sam Hocevar <sam@hocevar.net>
2012-11-22 08:31:18 +0000

    core: make hash operators const.

--- src/lol/core/hash.h
+++ src/lol/core/hash.h
@@ -22,30 +22,30 @@ namespace lol
 
 template<typename T> class Hash;
 
-template<> class Hash<int8_t>   { public: uint32_t operator()(int8_t); };
-template<> class Hash<uint8_t>  { public: uint32_t operator()(uint8_t); };
-template<> class Hash<int16_t>  { public: uint32_t operator()(int16_t); };
-template<> class Hash<uint16_t> { public: uint32_t operator()(uint16_t); };
-template<> class Hash<int32_t>  { public: uint32_t operator()(int32_t); };
-template<> class Hash<uint32_t> { public: uint32_t operator()(uint32_t); };
-template<> class Hash<int64_t>  { public: uint32_t operator()(int64_t); };
-template<> class Hash<uint64_t> { public: uint32_t operator()(uint64_t); };
+template<> class Hash<int8_t>   { public: uint32_t operator()(int8_t) const; };
+template<> class Hash<uint8_t>  { public: uint32_t operator()(uint8_t) const; };
+template<> class Hash<int16_t>  { public: uint32_t operator()(int16_t) const; };
+template<> class Hash<uint16_t> { public: uint32_t operator()(uint16_t) const; };
+template<> class Hash<int32_t>  { public: uint32_t operator()(int32_t) const; };
+template<> class Hash<uint32_t> { public: uint32_t operator()(uint32_t) const; };
+template<> class Hash<int64_t>  { public: uint32_t operator()(int64_t) const; };
+template<> class Hash<uint64_t> { public: uint32_t operator()(uint64_t) const; };
 
-template<> class Hash<half>     { public: uint32_t operator()(half); };
-template<> class Hash<float>    { public: uint32_t operator()(float); };
-template<> class Hash<double>   { public: uint32_t operator()(double); };
-template<> class Hash<ldouble>  { public: uint32_t operator()(ldouble); };
+template<> class Hash<half>     { public: uint32_t operator()(half) const; };
+template<> class Hash<float>    { public: uint32_t operator()(float) const; };
+template<> class Hash<double>   { public: uint32_t operator()(double) const; };
+template<> class Hash<ldouble>  { public: uint32_t operator()(ldouble) const; };
 
 template<> class Hash<char const *>
 {
 public:
-    uint32_t operator()(char const *x);
+    uint32_t operator()(char const *x) const;
 };
 
 template<> class Hash<String>
 {
 public:
-    uint32_t operator()(String const &s);
+    uint32_t operator()(String const &s) const;
 };
 
 } /* namespace lol */
