924a2afbbe9a7e9634ffa5bf9a05993f49e6565b
Benjamin ‘Touky’ Huet <huet.benjamin@gmail.com>
2012-12-18 21:59:23 +0000

    the stupidest idea of the day : the cube light, specular still missing.

--- src/easymesh/easymesh.h
+++ src/easymesh/easymesh.h
@@ -101,7 +101,7 @@ private:
     {
         Shader *shader;
         ShaderAttrib coord, norm, color;
-        ShaderUniform modelview, view, proj, normalmat, damage;
+        ShaderUniform modelview, view, invview, proj, normalmat, damage;
         VertexDeclaration *vdecl;
         VertexBuffer *vbo;
         IndexBuffer *ibo;
