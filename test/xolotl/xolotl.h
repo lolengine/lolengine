758f9f58e1ad1855c315a0559e7ef113964f6266
Sam Hocevar <sam@hocevar.net>
2012-12-29 03:07:49 +0000

    build: fix the WTFPL site URL in all code comments.

--- test/xolotl/xolotl.h
+++ test/xolotl/xolotl.h
@@ -6,7 +6,7 @@
 //   This program is free software; you can redistribute it and/or
 //   modify it under the terms of the Do What The Fuck You Want To
 //   Public License, Version 2, as published by Sam Hocevar. See
-//   http://sam.zoy.org/projects/COPYING.WTFPL for more details.
+//   http://www.wtfpl.net/ for more details.
 //
 
 using namespace lol;
