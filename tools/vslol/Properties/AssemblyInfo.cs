b42ec4b5c885bca558af15bea1dccbc8d479db27
Sam Hocevar <sam@hocevar.net>
2013-09-20 15:30:48 +0000

    vslol: mark version 1.0.2.0.

--- tools/vslol/Properties/AssemblyInfo.cs
+++ tools/vslol/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("58968f91-edb8-4a4c-9f4f-ba39fdb4a21a")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
