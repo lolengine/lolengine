18869ca359399e985eee9b22d68248af95fe22c0
Benjamin ‘Touky’ Huet <huet.benjamin@gmail.com>
2013-10-01 20:17:50 +0000

    Added Scene setup logic

---
+++ test/scenesetup.js
@@ -0,0 +1,19 @@
+//-------------------------------------------------------------------------
+//Command vars operations
+//-------------------------------------------------------------------------
+CmdVar("[ ]", ["Opens a new context,\nevery operations between the braces\nwill only be done to the vertices added\nsince the start of this context."]);
+CmdVar("float", ["Fraction value", "0.0"]);
+CmdVar("int", ["No decimal value", "0"]);
+CmdVar("bool", ["Boolean value", "true/false", "1/0"]);
+CmdVar("color", ["Red/Green/Blue/{Alpha}",
+                "HEXA: #ABC{D}", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;#AABBCC{DD}",
+                "FLOAT:&nbsp;f f f f", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(f f f f)", "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(f)"]);
+CmdVar("vec3", ["X/Y/Z as float", "&nbsp;f f f", "(f f f)", "(f)"]);
+
+//-------------------------------------------------------------------------
+//scene operations
+//-------------------------------------------------------------------------
+CmdType(["addlight"], "Add a light to the scene.\nUse other commands after this one to fully setup.", [CmdArg("float", "type")]);
+CmdType(["position"], "Set a position.\nWhen put after a light, sets its position.", [CmdArg("vec3", "pos")]);
+CmdType(["color"],    "Set a color.", [CmdArg("color", "color")]);
+
