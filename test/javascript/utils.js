47ca78b85ee85514fd970095a897b8ccc4e509b6
Benjamin ‘Touky’ Huet <huet.benjamin@gmail.com>
2013-10-08 00:01:04 +0000

    Meshviewer : web version now creates cookie to store current session.

--- test/javascript/utils.js
+++ test/javascript/utils.js
@@ -1,3 +1,85 @@
+//-----------------------------------------------------------------------------
+// COOKIE MANAGEMENT - nom nom nom nom -
+//-----------------------------------------------------------------------------
+
+function LolCookie() { }
+function LolCookieDays(m_name, m_value, m_expire_days)
+{
+    var cookie = new LolCookie();
+
+    cookie.m_name = m_name;
+    cookie.m_value = m_value;
+    cookie.m_expire_date = new Date();
+    cookie.m_expire_date.setDate(cookie.m_expire_date.getDate() + m_expire_days);
+
+    return cookie;
+}
+function LolCookieDate(m_name, m_value, m_expire_date)
+{
+    var cookie = new LolCookie();
+
+    cookie.m_name = m_name;
+    cookie.m_value = m_value;
+    cookie.m_expire_date = m_expire_date;
+
+    return cookie;
+}
+
+//Set a cookie
+function StoreLolCookie(cookie)
+{
+    //Get the cookie and removes it if it exists.
+    GetLolCookie(cookie.m_name, true);
+
+    var enc_value = escape(cookie.m_value) + ";";
+
+    //Add expire days
+    if (cookie.m_expire_date)
+        enc_value += " expires=" + cookie.m_expire_date.toUTCString() + ";";
+    enc_value = cookie.m_name + "=" + enc_value;
+
+    //Store cookies
+    document.cookie = enc_value;
+}
+
+//Get a cookie
+function GetLolCookie(name, remove)
+{
+    var cki_doc = document.cookie;
+    var cookie = null;
+
+    var cki_start = cki_doc.indexOf(name + "=");
+    if (cki_start > -1)
+    {
+        cookie = new LolCookie();
+        cookie.m_name = name;
+        cookie.m_expire_date = null;
+
+        //Retrieve value
+        var val_start = cki_doc.indexOf("=", cki_start) + 1;
+        var val_end = cki_doc.indexOf(";", cki_start);
+        cookie.m_value = unescape(cki_doc.substring(val_start, val_end));
+
+        if (remove)
+            document.cookie = name + "=; expires=0;";
+    }
+    return cookie;
+}
+
+//Check the existence of a cookie
+function DoesLolCookieExist(name)
+{
+    var cki_doc = document.cookie;
+    if (cki_doc.indexOf(name + "=") > -1)
+        return true;
+    return false;
+}
+
+//-----------------------------------------------------------------------------
+// FILE MANAGEMENT
+//-----------------------------------------------------------------------------
+
+//Dynamic load
 function DynLoadFile(filename)
 {
     var filetype = filename.match(/\.[a-zA-Z]+$/);
@@ -20,6 +102,7 @@ function DynLoadFile(filename)
         document.getElementsByTagName("head")[0].appendChild(fileref);
 }
 
+//Dynamic remove
 function DynRemoveFile(filename)
 {
     var filetype = filename.match(/\.[a-zA-Z]+$/);
