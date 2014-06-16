d5012b14fe29555879e391846417b58c0fd16dd5
Sam Hocevar <sam@hocevar.net>
2014-06-16 23:33:30 +0000

    image: start to implement some conversions between pixel spaces.

--- src/image/image.cpp
+++ src/image/image.cpp
@@ -1,7 +1,7 @@
 //
 // Lol Engine
 //
-// Copyright: (c) 2010-2013 Sam Hocevar <sam@hocevar.net>
+// Copyright: (c) 2010-2014 Sam Hocevar <sam@hocevar.net>
 //   This program is free software; you can redistribute it and/or
 //   modify it under the terms of the Do What The Fuck You Want To
 //   Public License, Version 2, as published by Sam Hocevar. See
@@ -167,30 +167,6 @@ void Image::SetSize(ivec2 size)
     {
         Unlock(Lock<PixelFormat::RGBA_8>());
     }
-}
-
-PixelFormat Image::GetFormat() const
-{
-    return m_data->m_format;
-}
-
-void Image::SetFormat(PixelFormat fmt)
-{
-    /* If we never used this format, allocate a new buffer: we will
-     * obviously need it. */
-    if (m_data->m_pixels[(int)fmt] == nullptr)
-    {
-        int bytes = m_data->m_size.x * m_data->m_size.y * BytesPerPixel(fmt);
-        m_data->m_pixels[(int)fmt] = new uint8_t[bytes];
-    }
-
-    /* If the requested format is different from the current format, and if
-     * the current format is a valid format, convert pixels. */
-    if (fmt != m_data->m_format && m_data->m_format != PixelFormat::Unknown)
-    {
-        /* TODO: convert data */
-    }
-    m_data->m_format = fmt;
 }
 
 /* The Lock() method */
