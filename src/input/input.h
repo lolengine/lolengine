07ffc8139086768f1a37b1740c12760ee7742530
Sam Hocevar <sam@hocevar.net>
2014-09-11 13:09:03 +0000

    input: use scancodes instead of ASCII values when using SDL v2.

--- src/input/input.h
+++ src/input/input.h
@@ -20,43 +20,103 @@ class InputDevice
 {
 public:
     /** Gets the name of this input device */
-    const String& GetName() const { return m_name; }
+    const String& GetName() const
+    {
+        return m_name;
+    }
 
     /** Gets the index of the corresponding key, needed to call GetKey */
-    ptrdiff_t GetKeyIndex(const char* name) const { return GetItemIndex(name, m_keynames); }
+    ptrdiff_t GetKeyIndex(const char* name) const
+    {
+        return GetItemIndex(name, m_keynames);
+    }
+
     /** Gets the index of the corresponding axis, needed to call GetAxis */
-    ptrdiff_t GetAxisIndex(const char* name) const { return GetItemIndex(name, m_axisnames); }
+    ptrdiff_t GetAxisIndex(const char* name) const
+    {
+        return GetItemIndex(name, m_axisnames);
+    }
+
     /** Gets the index of the corresponding cursor, needed to call GetCursor */
-    ptrdiff_t GetCursorIndex(const char* name) const { return GetItemIndex(name, m_cursornames); }
+    ptrdiff_t GetCursorIndex(const char* name) const
+    {
+        return GetItemIndex(name, m_cursornames);
+    }
 
-    /** Gets the current state of the given key, true being pressed and false being released */
-    bool GetKey(ptrdiff_t index) const { return m_keys[index]; }
-    /** Gets the current value of the given axis. Devices should cap this value between -1 and 1 as much as possible, through it is not guaranteed */
-    float GetAxis(ptrdiff_t index) const { return m_axis[index].m1 * m_axis[index].m2; }
-    /** Gets the current value of the given cursor, 0,0 being the bottom-left corner and 1,1 being the top-right corner */
-    vec2 GetCursor(ptrdiff_t index) const { return m_cursors[index].m1; }
-    /** Gets the coordinate of the pixel the cursor is currently over, 0,0 being the bottom-left corner. */
-    ivec2 GetCursorPixel(ptrdiff_t index) const { return m_cursors[index].m2; }
+    /** Gets the current state of the given key, true being pressed and
+      * false being released */
+    bool GetKey(ptrdiff_t index) const
+    {
+        return m_keys[index];
+    }
 
+    /** Gets the current value of the given axis. Devices should try to
+      * clamp this value between -1 and 1, though it is not guaranteed. */
+    float GetAxis(ptrdiff_t index) const
+    {
+        return m_axis[index].m1 * m_axis[index].m2;
+    }
 
-    /** Sets a per-device-axis sensitivity factor. The value returned by the operating system will be multiplied by this value before being returned by GetAxis */
-    void SetAxisSensitivity(ptrdiff_t index, float sensitivity) { m_axis[index].m2 = sensitivity; }
-    /** Gets the per-device-axis sensitivity factor. The value returned by the operating system will be multiplied by this value before being returned by GetAxis */
-    float GetAxisSensitivity(ptrdiff_t index) const { return m_axis[index].m2; }
+    /** Gets the current value of the given cursor, 0,0 being the bottom-left
+      * corner and 1,1 being the top-right corner */
+    vec2 GetCursor(ptrdiff_t index) const
+    {
+        return m_cursors[index].m1;
+    }
+
+    /** Gets the coordinate of the pixel the cursor is currently over,
+      * 0,0 being the bottom-left corner. */
+    ivec2 GetCursorPixel(ptrdiff_t index) const
+    {
+        return m_cursors[index].m2;
+    }
+
+    /** Sets a per-device-axis sensitivity factor. The value returned by
+      * the operating system will be multiplied by this value before being
+      * returned by GetAxis */
+    void SetAxisSensitivity(ptrdiff_t index, float sensitivity)
+    {
+        m_axis[index].m2 = sensitivity;
+    }
+
+    /** Gets the per-device-axis sensitivity factor. The value returned by
+      * the operating system will be multiplied by this value before being
+      * returned by GetAxis */
+    float GetAxisSensitivity(ptrdiff_t index) const
+    {
+        return m_axis[index].m2;
+    }
 
     /** Gets a list of the name of all available keys in this device */
-    const array<String>& GetAllKeys() const { return m_keynames; }
+    const array<String>& GetAllKeys() const
+    {
+        return m_keynames;
+    }
     /** Gets a list of the name of all available axis in this device */
-    const array<String>& GetAllAxis() const { return m_axisnames; }
+    const array<String>& GetAllAxis() const
+    {
+        return m_axisnames;
+    }
     /** Gets a list of the name of all available cursors in this device */
-    const array<String>& GetAllCursors() const { return m_cursornames; }
+    const array<String>& GetAllCursors() const
+    {
+        return m_cursornames;
+    }
 
     /** Gets a list of the name of all available input devices */
     static array<String> GetAvailableDevices();
+
     /** Gets an input device by its name */
-    static InputDevice* Get(const char* name) { return GetDevice(name); }
+    static InputDevice* Get(const char* name)
+    {
+        return GetDevice(name);
+    }
+
     /** Sets whether the mouse cursor should be captured. */
-    static void CaptureMouse(bool activated) { m_capturemouse = activated; }
+    static void CaptureMouse(bool activated)
+    {
+        m_capturemouse = activated;
+    }
 
 protected:
     // TODO: hide all of this in a InputDeviceData?
@@ -69,8 +129,10 @@ protected:
 
     /** key states (pressed/released) */
     array<bool> m_keys;
+
     /** axis states (value and sensitivity) */
     array<float, float> m_axis;
+
     /** cursor position */
     array<vec2, ivec2> m_cursors;
 
@@ -97,11 +159,11 @@ private:
     static array<InputDevice*> devices;
 
     template <typename... T>
-    ptrdiff_t GetItemIndex(const char* name, const array<String, T...>& array) const
+    ptrdiff_t GetItemIndex(const char* name, const array<String, T...>& a) const
     {
-        for (ptrdiff_t i = 0; i < array.Count(); ++i)
+        for (ptrdiff_t i = 0; i < a.Count(); ++i)
         {
-            if (array[i] == name)
+            if (a[i] == name)
                 return i;
         }
         return -1;
