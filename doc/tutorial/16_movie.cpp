ad45e4d284c8e9213add8a916ed0442b6148276b
Sam Hocevar <sam@hocevar.net>
2016-10-23 17:00:09 +0200

    doc: fix linking with ffmpeg on Linux and start a tutorial program.

---
+++ doc/tutorial/16_movie.cpp
@@ -0,0 +1,25 @@
+//
+//  Lol Engine — Movie tutorial
+//
+//  Copyright © 2016 Sam Hocevar <sam@hocevar.net>
+//
+//  Lol Engine is free software. It comes without any warranty, to
+//  the extent permitted by applicable law. You can redistribute it
+//  and/or modify it under the terms of the Do What the Fuck You Want
+//  to Public License, Version 2, as published by the WTFPL Task Force.
+//  See http://www.wtfpl.net/ for more details.
+//
+
+#if HAVE_CONFIG_H
+#   include "config.h"
+#endif
+
+#include <lol/engine.h>
+
+using namespace lol;
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
+
