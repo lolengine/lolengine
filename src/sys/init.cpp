c8c78762aadb0851eb98e14627240d49fff53bd9
Sam Hocevar <sam@hocevar.net>
2024-02-04 08:13:08 +0100

    sys: clean up usage of std::filesystem

--- src/sys/init.cpp
+++ src/sys/init.cpp
@@ -16,7 +16,7 @@
 #include <vector> // std::vector
 #include <string> // std::string
 #include <cctype>
-#if !__ANDROID__ && !__EMSCRIPTEN__ && !__NX__
+#if !defined _LIBCPP_HAS_NO_FILESYSTEM_LIBRARY
 #   include <filesystem> // std::filesystem::exists
 #endif
 
@@ -25,13 +25,9 @@
 namespace lol::sys
 {
 
-#if _WIN32
-#   define SEPARATOR '\\'
-#else
-#   define SEPARATOR '/'
+#if !defined _LIBCPP_HAS_NO_FILESYSTEM_LIBRARY
+static std::vector<std::filesystem::path> data_dir;
 #endif
-
-static std::vector<std::string> data_dir;
 
 void init(int argc, char *argv[],
           std::string const &name, int width, int height,
@@ -45,28 +41,18 @@ void init(int argc, char *argv[],
     kinc_init(name.c_str(), width, height, nullptr, nullptr);
 #endif
 
-    msg::debug("project dir: “%s”\n", projectdir.c_str());
-    msg::debug("solution dir: “%s”\n", solutiondir.c_str());
-    msg::debug("source subdir: “%s”\n", sourcesubdir.c_str());
+#if !defined _LIBCPP_HAS_NO_FILESYSTEM_LIBRARY
+    msg::debug("project dir: “%s”\n", std::filesystem::path(projectdir).generic_string().c_str());
+    msg::debug("solution dir: “%s”\n", std::filesystem::path(solutiondir).generic_string().c_str());
+    msg::debug("source subdir: “%s”\n", std::filesystem::path(sourcesubdir).generic_string().c_str());
 
     //
-    // Retrieve binary directory, defaulting to no directory on Android
-    // and emscripten, and the current directory on other platforms.
+    // Retrieve the binary directory, defaulting to current directory if no
+    // reasonable guess can be made.
     //
 
-#if __ANDROID__ || __EMSCRIPTEN__ || __NX__
-    std::string binarydir = "";
-#else
-    std::string binarydir = std::filesystem::current_path().string() + SEPARATOR;
-
-    if (argc > 0)
-    {
-        char const *last_sep = strrchr(argv[0], SEPARATOR);
-        if (last_sep)
-            binarydir = std::string(argv[0], last_sep - argv[0] + 1);
-    }
-#endif
-
+    auto binarydir = argc > 0 ? std::filesystem::path(argv[0]).parent_path()
+                              : std::filesystem::current_path();
     bool got_rootdir = false;
 
     //
@@ -77,32 +63,23 @@ void init(int argc, char *argv[],
     if (!got_rootdir && projectdir.length() && solutiondir.length())
     {
         // This data dir is for standalone executables
-        std::string rootdir = binarydir;
-#if !__NX__
-        if (rootdir.length() && rootdir.back() != SEPARATOR)
-            rootdir += SEPARATOR;
-        add_data_dir(rootdir);
-#endif
+        if (std::filesystem::is_directory(binarydir))
+            data_dir.push_back(binarydir);
 
         // This data dir is for engine stuff
-        rootdir = solutiondir;
-        if (rootdir.length() && rootdir.back() != SEPARATOR)
-            rootdir += SEPARATOR;
-        rootdir += "../src/"; // FIXME: use SEPARATOR?
-        add_data_dir(rootdir);
+        auto rootdir = std::filesystem::path(solutiondir).parent_path() / "src";
+        if (std::filesystem::is_directory(rootdir))
+            data_dir.push_back(rootdir);
 
         // This data dir is for submodule support stuff
-        rootdir = solutiondir;
-        if (rootdir.length() && rootdir.back() != SEPARATOR)
-            rootdir += SEPARATOR;
-        rootdir += "./lol/src/"; // FIXME: use SEPARATOR?
-        add_data_dir(rootdir);
+        rootdir = std::filesystem::path(solutiondir) / "lol" / "src";
+        if (std::filesystem::is_directory(rootdir))
+            data_dir.push_back(rootdir);
 
         // This data dir is for project-specific stuff
-        rootdir = projectdir;
-        if (rootdir.length() && rootdir.back() != SEPARATOR)
-            rootdir += SEPARATOR;
-        add_data_dir(rootdir);
+        rootdir = std::filesystem::path(projectdir);
+        if (std::filesystem::is_directory(rootdir))
+            data_dir.push_back(rootdir);
 
         got_rootdir = true;
     }
@@ -115,30 +92,29 @@ void init(int argc, char *argv[],
     {
         // First climb back the hierarchy to get to the engine root and
         // add a data dir for engine stuff.
-        std::string rootdir = binarydir;
-        if (rootdir.length() && rootdir.back() != SEPARATOR)
-            rootdir += SEPARATOR;
-        for (int i = 1; i < (int)sourcesubdir.length(); ++i)
+        auto rootdir = binarydir;
+        auto subdir = std::filesystem::path(sourcesubdir);
+
+        while (subdir.parent_path() != subdir)
         {
-            if ((sourcesubdir[i] == SEPARATOR
-                  && sourcesubdir[i - 1] != SEPARATOR)
-                 || i == (int)sourcesubdir.length() - 1)
-                rootdir += "../";
+            rootdir = rootdir.parent_path();
+            subdir = subdir.parent_path();
         }
-        rootdir += "src/";
-        add_data_dir(rootdir);
+
+        data_dir.push_back(rootdir / "src");
 
         // This data dir is for project-specific stuff
-        rootdir = binarydir;
-        add_data_dir(rootdir);
+        if (std::filesystem::is_directory(binarydir))
+            data_dir.push_back(std::filesystem::path(binarydir));
 
         got_rootdir = true;
     }
 
-    msg::debug("binary dir: “%s”\n", binarydir.c_str());
+    msg::debug("binary dir: “%s”\n", binarydir.generic_string().c_str());
     for (int i = 0; i < int(data_dir.size()); ++i)
         msg::debug("data dir %d/%d: “%s”\n", i + 1, int(data_dir.size()),
-                   data_dir[i].c_str());
+                   data_dir[i].generic_string().c_str());
+#endif
 }
 
 void run()
@@ -161,22 +137,24 @@ void stop()
 
 void add_data_dir(std::string const &dir)
 {
+#if !defined _LIBCPP_HAS_NO_FILESYSTEM_LIBRARY
     data_dir.push_back(dir);
+#endif
 }
 
 std::string get_data_path(std::string const &file)
 {
+#if !defined _LIBCPP_HAS_NO_FILESYSTEM_LIBRARY
     // If not an absolute path, look through known data directories
     if (file[0] != '/')
         for (auto const &dir : data_dir)
         {
-            auto path = dir + file;
-#if !__ANDROID__ && !__EMSCRIPTEN__ && !__NX__
+            auto path = dir / file;
             std::error_code ec;
             if (std::filesystem::exists(path, ec))
-                return path;
-#endif
+                return path.generic_string();
         }
+#endif
 
     return file;
 }
